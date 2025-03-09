b16eba5a77b984954c36cd8404ce2d436980e6c9
Kelvin Chow <workinghotdog@gmail.com>
2025-03-10 04:02:39 +0800

    added new plugins

---
+++ hello_plugin/hello_plugin.py
@@ -0,0 +1,6 @@
+def initialize():
+    print("Hello plugin initialized!")
+
+
+def do_task():
+    print("Hello plugin is doing a task.")
---
+++ report_plugin/report_plugin.py
@@ -0,0 +1,9 @@
+# report_plugin.py
+def initialize():
+    print("Report plugin has been initialized.")
+
+def do_task():
+    print("Hello plugin is doing a task.")
+
+def generate_report():
+    return "This is a generated report."
